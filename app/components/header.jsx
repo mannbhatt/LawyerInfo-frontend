d067ec6c107e785f714f9e6a6e25c6bd0f6ada9d
Mann Bhatt <120254618+mannbhatt@users.noreply.github.com>
2025-03-09 14:13:28 +0530

    Update header.jsx

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -86,6 +86,9 @@ export default function Header() {
     localStorage.clear()
     setUser(null)
     router.push("/login")
+    setTimeout(() => {
+    window.location.reload(); // Reload the page to clear client-side state
+  }, 100);
   }
 
   return (
